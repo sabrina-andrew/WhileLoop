03104c34b9d2d232b60cad36cff57eddade7dabb
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-29 20:45:44 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pickANumber = 1;
+            
+
+            while (pickANumber <= 100)
+            {
+                Console.WriteLine(pickANumber);
+                pickANumber++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
